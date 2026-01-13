4e2a42cd02dbd1ea8ac7495249fd8426d2287b40
Wyliam <wyliam.burger@epitech.eu>
2026-01-13 01:19:09 +0100

    fix: Folder restructuration

---
+++ README.md
@@ -0,0 +1,2 @@
+The JSON datasheet is brought by **Bowserinator**
+Check the original repository [here](https://github.com/Bowserinator/Periodic-Table-JSON)
--- app/main.py
+++ app/main.py
@@ -2,13 +2,19 @@ import json
 from typing import Union
 from fastapi import FastAPI, HTTPException
 
+# Making JSON Data File Path Dynamic
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+json_path = os.path.join(BASE_DIR, "data", "PeriodicTableJSON.json")
+
 app = FastAPI(title="Promethium", version="1.0.0")
 
-with open("data/PeriodicTableJSON.json", "r", encoding="utf-8") as file:
+# Load JSON data
+with open(json_path, "r", encoding="utf-8") as file:
     elements_data = json.load(file) 
 
+# Root endpoint
 @app.get("/", tags=["Root"])
 def welcome_root():
     return {"message": "Welcome to the Promethium API! Don't forget to visit /docs for API documentation."}
-
 
